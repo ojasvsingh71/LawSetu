5c6aaef32891e28f17b536612aa3956279aacb9b
Ankit-Kumar28 <ankitk123907@gmail.com>
2025-06-17 18:37:12 +0530

    check code edit App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,13 @@ function App() {
 
     return (
         <div className="w-full min-h-screen p-0 bg-indigo-400 flex flex-col items-center justify-center gap-4">
-            <h1 className="text-2xl text-white font-bold">LawSetu</h1>
-            <button className="bg-black text-white text-xl px-4 py-2 rounded" onClick={()=>setCount((count)=> count+1)}>
-                Click me {count} 
+            <h1 className="text-4xl text-white font-bold">LawSetu</h1>
+            <h2 className="font-bold text-2xl">value : {count}</h2>
+            <button className="bg-black text-white text-xl px-4 py-2 rounded" onClick={()=>setCount(count+1)}>
+               Click for PLUS 
+            </button> <br />
+             <button className="bg-black text-white text-xl px-4 py-2 rounded" onClick={()=>setCount(count-1)}>
+               Click for MINUS 
             </button>
         </div>
     );
