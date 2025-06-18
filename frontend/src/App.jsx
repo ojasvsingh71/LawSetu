2d2329ea63a41ff531fb47ec4b1dc04ddc1343d8
Ankit-Kumar28 <ankitk123907@gmail.com>
2025-06-18 20:37:41 +0530

    Added Home & Login pages, updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
-
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home"; 
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const [count, setCount] = useState(0);
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/home" />
+          ) : (
+            <Login setIsAuthenticated={setIsAuthenticated} />
+          )
+        }
+      />
 
-    return (
-        <div className="w-full min-h-screen p-0 bg-indigo-400 flex flex-col items-center justify-center gap-4">
-            <h1 className="text-4xl text-white font-bold">LawSetu</h1>
-            <h2 className="font-bold text-2xl">value : {count}</h2>
-            <button className="bg-black text-white text-xl px-4 py-2 rounded" onClick={()=>setCount(count+1)}>
-               Click for PLUS 
-            </button> <br />
-             <button className="bg-black text-white text-xl px-4 py-2 rounded" onClick={()=>setCount(count-1)}>
-               Click for MINUS 
-            </button>
-        </div>
-    );
+      <Route
+        path="/home"
+        element={
+          isAuthenticated ? (
+            <Home />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="w-full min-h-screen p-0 bg-indigo-400 flex flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl text-white font-bold">LawSetu</h1>
+      <h2 className="font-bold text-2xl">🏠 Welcome to Home Page!</h2>
+    </div>
+  );
+};
+
+export default Home;
