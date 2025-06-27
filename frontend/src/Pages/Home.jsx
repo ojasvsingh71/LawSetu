48248332949c35ece896ed2013e86caf842fd20e
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-06-27 17:41:23 +0530

    Fixed to tailwind V4 and fixed bg-toggle

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="w-full min-h-screen p-0 bg-indigo-400 flex flex-col items-center justify-center gap-4">
       <h1 className="text-4xl text-white font-bold">LawSetu</h1>
       <h2 className="font-bold text-2xl">🏠 Welcome to Home Page!</h2>
+      <button className="p-3 bg-blue-400 border border-white rounded-lg" onClick={() => {
+        navigate("/editor")
+      }}>Editor</button>
     </div>
   );
 };
