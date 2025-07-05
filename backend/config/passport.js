8012e32d013b2e565f58b5d02c39629639842193
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-07-05 18:26:14 +0530

    callbackURL

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ import userModel from "../models/user.model.js";
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://lawsetu.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const email = profile.emails[0].value;
